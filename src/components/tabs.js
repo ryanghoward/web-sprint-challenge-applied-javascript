296fd21895ec2edb5d7182d53d5328dc5f890567
Ryan Howard <ryanghoward@protonmail.com>
2021-10-29 10:58:25 -0700

    Task 4 in progress. Response data console logging correctly

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -36,6 +38,14 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios.get('http://localhost:5000/api/topics')
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err);
+    })
 }
 
 export { Tabs, tabsAppender }
